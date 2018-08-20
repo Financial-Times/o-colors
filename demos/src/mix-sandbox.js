7cdf52fbbd209a56013d975e0cd55e3782f550ff
Gabrielle von Koss <gabivk@gmail.com>
2018-08-20 12:04:27 +0100

    cleanup, sorta

--- demos/src/mix-sandbox.js
+++ demos/src/mix-sandbox.js
@@ -6,67 +6,49 @@ document.addEventListener('DOMContentLoaded', function() {
 
 	mixer.addEventListener('change', () => {
 		oColorsMix(mixer.value, base.value);
-		let hex = document.querySelector('[aria-selected=true]');
-		let percent = hex.parentElement.classList.value.replace('percent-','');
-		fillCodeSnippets(hex.innerText, mixer.value, base.value, percent);
+		changeCodeSnippetValues(mixer.value, base.value);
 	});
 
 	base.addEventListener('change', () => {
 		oColorsMix(mixer.value, base.value);
-
-		let hex = document.querySelector('[aria-selected=true]');
-		let percent = hex.parentElement.classList.value.replace('percent-','');
-		fillCodeSnippets(hex.innerText, mixer.value, base.value, percent);
+		changeCodeSnippetValues(mixer.value, base.value);
 	});
 
 	oColorsMix(mixer.value, base.value);
+
+	//set visible hex value and sass function to default values of oColorsMix
 	document.querySelector('.percent-80').firstElementChild.setAttribute('aria-selected', true);
 	let defaultHex = getComputedStyle(document.documentElement).getPropertyValue(`--o-colors-${mixer.value}-80`);
 	fillCodeSnippets(defaultHex, mixer.value, base.value, 80);
 });
 
-function oColorsMix(mixer = 'black', base = 'paper') {
+const changeCodeSnippetValues = (mixer, base) => {
+	let hex = document.querySelector('[aria-selected=true]');
+	let percent = hex.parentElement.classList.value.replace('percent-','');
+	fillCodeSnippets(hex.innerText, mixer, base, percent);
+}
+
+const oColorsMix = (mixer = 'black', base = 'paper') => {
 	const mixerHex = getComputedStyle(document.documentElement).getPropertyValue(`--o-colors-${mixer}`).replace(/^\s*#/,'');
 	const baseHex = getComputedStyle(document.documentElement).getPropertyValue(`--o-colors-${base}`).replace(/^\s*#/,'');
 	const textColorRGB = getComputedStyle(document.body).getPropertyValue('color');
 
-	let textColor = textColorRGB === 'rgb(0, 0, 0)' ? '#000000' : '#f3f3f3';
+	checkContrast(textColorRGB, baseHex);
+	colourSwatches(mixHexes(mixerHex, baseHex));
+}
 
-	let ratio = contrastRatio.oColorsGetContrastRatio(textColor, baseHex);
+const checkContrast = (text, background) => {
+	let textColor = text === 'rgb(0, 0, 0)' ? '#000000' : '#f3f3f3';
+
+	let ratio = contrastRatio.oColorsGetContrastRatio(text, background);
 
 	if (ratio <= 3) { //if it fails accessbility
 		textColor = textColor === '#000000' ? '#f3f3f3' : '#000000';
 		document.body.style.setProperty('--color', textColor);
 	}
-
-	const mixHex = mixColors(mixerHex, baseHex);
-
-	mixHex.forEach((hex, index) => {
-		let range = document.querySelector('.mix-range');
-		let swatch = range.querySelector(`.percent-${index}0 .sqr`);
-		swatch.style.backgroundColor = swatch.style.color = swatch.innerText = hex;
-
-		if (index === 0) {
-			document.body.style.backgroundColor = hex;
-		}
-
-		swatch.addEventListener('click', () => {
-			range.querySelectorAll(`.sqr`).forEach(sqr => {
-				sqr.setAttribute('aria-selected', false);
-			});
-
-			swatch.setAttribute('aria-selected', true);
-			fillCodeSnippets(hex, mixer, base, index * 10);
-		});
-	});
 }
 
-const fillCodeSnippets = (hex, mixer, base, index) => {
-	document.getElementById('hex-value').innerText = hex;
-	document.getElementById('code-snippet').innerText = `oColorsMix(${mixer}, ${base}, ${index})`;
-};
-
-function mixColors(mixer, base) {
+const mixHexes = (mixer, base) => {
 	const radix = 16;
 	const decimalToHex = decimal => decimal.toString(radix);
 	const hexToDecimal = hex => parseInt(hex, radix);
@@ -92,3 +74,30 @@ function mixColors(mixer, base) {
 		return hexValue;
 	});
 }
+
+
+const colourSwatches = (hexes) => {
+	hexes.forEach((hex, index) => {
+		let range = document.querySelector('.mix-range');
+		let swatch = range.querySelector(`.percent-${index * 10} .sqr`);
+		swatch.style.backgroundColor = swatch.style.color = swatch.innerText = hex;
+
+		if (index === 0) {
+			document.body.style.backgroundColor = hex;
+		}
+
+		swatch.addEventListener('click', () => {
+			range.querySelectorAll(`.sqr`).forEach(sqr => {
+				sqr.setAttribute('aria-selected', false);
+			});
+
+			swatch.setAttribute('aria-selected', true);
+			fillCodeSnippets(hex, mixer, base, index * 10);
+		});
+	});
+}
+
+const fillCodeSnippets = (hex, mixer, base, index) => {
+	document.getElementById('hex-value').innerText = hex;
+	document.getElementById('code-snippet').innerText = `oColorsMix(${mixer}, ${base}, ${index})`;
+};
