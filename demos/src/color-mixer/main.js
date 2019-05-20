adb6ff7701d3f4fef2be6a7ab8e995652736ea2a
Gabrielle von Koss <gabivk@gmail.com>
2019-05-20 13:24:13 +0100

    almost there

--- demos/src/color-mixer/main.js
+++ demos/src/color-mixer/main.js
@@ -2,30 +2,28 @@ import contrastRatio from '../sandboxes/contrast-ratio';
 import { mixHexes } from '../sandboxes/colors-mix';
 
 document.addEventListener('o.DOMContentLoaded', function() {
-	const form = document.forms[0];
-	const mixer = form['mixer'];
-	const base = form['base'];
+	const forms = document.forms;
+	const mixer = forms[0]['mixer'];
+	const base = forms[0]['base'];
 
-	form.addEventListener('change', () => {
+	forms[0].addEventListener('change', (e) => {
 		mixColors(mixer.value, base.value);
 		changeCodeSnippetValues(mixer.value, base.value);
 	});
 
-	// base.addEventListener('change', () => {
-	// 	mixColors(mixer.value, base.value);
-	// 	changeCodeSnippetValues(mixer.value, base.value);
-	// });
-
 	mixColors(mixer.value, base.value);
 
 	//set visible hex value and sass function to default values of mixColors
-	document.querySelector('.percent-80').firstElementChild.setAttribute('aria-selected', true);
-	let defaultHex = getComputedStyle(document.documentElement).getPropertyValue(`--o-colors-${mixer.value}-80`);
-	fillCodeSnippets(defaultHex, mixer.value, base.value, 80);
+	forms[1]['range'][5].checked = true;
+	// document.querySelector('.percent-80').firstElementChild.setAttribute('aria-selected', true);
+	let defaultHex = getComputedStyle(document.documentElement).getPropertyValue(`--o-colors-${mixer.value}-50`);
+	fillCodeSnippets(defaultHex, mixer.value, base.value, 50);
 });
 
 const changeCodeSnippetValues = (mixer, base) => {
-	let hex = document.querySelector('[aria-selected=true]');
+	console.log(document.forms[1]['range'].indexOf(document.forms[1]['range'].value));
+	let hex = document.querySelector('input[type=radio]:checked');
+	console.log(hex);
 	let percent = hex.parentElement.classList.value.replace('percent-', '');
 	fillCodeSnippets(hex.innerText, mixer, base, percent);
 };
@@ -33,7 +31,7 @@ const changeCodeSnippetValues = (mixer, base) => {
 const checkTextContrast = (background) => {
 	const text = getComputedStyle(document.documentElement).getPropertyValue('color');
 	let textColor = text === 'rgb(0, 0, 0)' ? '#000000' : '#f3f3f3';
-	
+	console.log({textColor})
 	const ratio = contrastRatio.oColorsGetContrastRatio(textColor, background);
 	if (ratio <= 3) { // if it fails accessbility, change text colour
 		textColor = textColor === '#000000' ? '#f3f3f3' : '#000000';
@@ -44,32 +42,21 @@ const checkTextContrast = (background) => {
 const mixColors = (mixer = 'black', base = 'paper') => {
 	let mixerHex = getComputedStyle(document.documentElement).getPropertyValue(`--o-colors-${mixer}`).replace(/^\s*#/, '')
 	let baseHex = getComputedStyle(document.documentElement).getPropertyValue(`--o-colors-${base}`).replace(/^\s*#/, '')
+	document.documentElement.style.setProperty('--background', `#${baseHex}`);
+
 	const hexArray = mixHexes(mixerHex, baseHex);
 	fillSwatches(hexArray, mixer, base);
 	checkTextContrast(baseHex);
 }
 
 const fillSwatches = (hexes, mixer, base) => {
+	const range = document.forms[1]['range'];
 	hexes.forEach((hex, index) => {
-		let range = document.querySelector('.mix-range');
+		let swatch = range[index];
+		swatch.style.backgroundColor = hex;
+		swatch.value = hex;
 
-		if (range) {
-			let swatch = range.querySelector(`.percent-${index * 10} .sqr`);
-			swatch.style.backgroundColor = swatch.style.color = swatch.innerText = hex;
-
-			if (index === 0) {
-				document.documentElement.style.setProperty('--background', hex)
-			}
-
-			swatch.addEventListener('click', () => {
-				range.querySelectorAll(`.sqr`).forEach(sqr => {
-					sqr.setAttribute('aria-selected', false);
-				});
-
-				swatch.setAttribute('aria-selected', true);
-				fillCodeSnippets(hex, mixer, base, index * 10);
-			});
-		}
+		swatch.addEventListener('click', (e) => fillCodeSnippets(e.target.value, mixer, base, index * 10))
 	});
 }
 
